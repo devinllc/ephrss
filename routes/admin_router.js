a624a543f42f07b374f625ac67d0697c30fdaa09
Ramesh Vishwakarma <rameshvishwakarma@Rameshs-MacBook-Air.local>
2026-03-22 19:25:24 +0530

    vercel fix

--- routes/admin_router.js
+++ routes/admin_router.js
@@ -1,6 +1,6 @@
 const exprees = require("express");
 const router = exprees.Router();
-const { adminSignup, adminLogin, adminLogout, updateLocation } = require("../controller/admin_auth_controller");
+const { adminSignup, adminLogin, adminLogout, updateLocation, getLocation } = require("../controller/admin_auth_controller");
 const isloginMiddleware = require("../middleware/isloginMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 const { resetDeviceId } = require("../controller/admin/reset_device_id_controller");
